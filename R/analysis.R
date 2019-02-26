c6b209eafcd1f3fc3105b67765a4618901d65c80
jasonvii <jasoon.vii@gmail.com>
2019-02-26 12:20:50 +0100

    drake exercice

--- R/analysis.R
+++ R/analysis.R
@@ -1,3 +1,7 @@
+library(rmarkdown)
+library(drake)
+library(lme4)
+
 #############
 # FUNCTIONS #
 #############
@@ -25,3 +29,20 @@ trend_line_continent_add  <-  function (x, y, data, continent, color_table) {
 f  <-  function (...) {
     trend_line_continent_add(x = 'gdpPercap', y = 'lifeExp', ...)
 }
+
+makeFigure1 <- function (data_1982, output_filename, ...) {
+    
+    color_table  <-  c(Asia = 'tomato', Europe = 'chocolate4', Africa = 'dodgerblue2', Americas = 'darkgoldenrod1', Oceania = 'green4')
+    col          <-  category_color(data_1982$continent, color_table)
+    cex          <-  rescale(sqrt(data_1982$pop), c(0.2, 10))
+    continents   <-  unique(data_1982$continent)
+    
+    pdf(output_filename, width = 7, height = 7)
+    plot(lifeExp ~ gdpPercap, data_1982, log = 'x', cex = cex, col = col, pch = 21, las = 1, xlab = 'GDP per capita', ylab = 'Life expectancy (years)', ...)
+    for (i in seq_along(continents)) {
+        f(data = data_1982, continent = continents[i], color_table = color_table)
+    }
+    dev.off()
+    
+}
+
---
+++ R/plan.R
@@ -0,0 +1,12 @@
+plan <- drake::drake_plan(
+    data       =  read.csv('data/gapminder-FiveYearData.csv', stringsAsFactors = FALSE),
+    data_1982  =  data[data$year == 1982, ],
+
+    model  =  lme4::lmer(log(lifeExp) ~ log(gdpPercap) + (1 + log(gdpPercap) | year), data = data),
+    out    =  coef(model), 
+    folder =  dir.create('output'),
+    
+    figure1 = makeFigure1(data_1982, output_filename = 'output/figure1.pdf', main = '1982'),
+    figure2 = makeFigure1(data, output_filename = 'output/figure2.pdf', main = 'whole time series')
+
+)
