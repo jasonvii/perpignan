37ade73f76636badd63fd5b4cd37eddf8b0647a0
jasonvii <jasoon.vii@gmail.com>
2019-02-26 11:40:50 +0100

    organize library

--- script.R
+++ script.R
@@ -1,4 +1,7 @@
+library(lme4)
+
 source('R/analysis.R')
+source('R/figures.R')
 
 ########
 # DATA #
@@ -13,17 +16,5 @@ library(lme4)
 model  <-  lme4::lmer(log(lifeExp) ~ log(gdpPercap) + (1 + log(gdpPercap) | year), data = data)
 out    <-  coef(model)
 
-########
-# PLOT #
-########
-color_table  <-  c(Asia = 'tomato', Europe = 'chocolate4', Africa = 'dodgerblue2', Americas = 'darkgoldenrod1', Oceania = 'green4')
-col          <-  category_color(data_1982$continent, color_table)
-cex          <-  rescale(sqrt(data_1982$pop), c(0.2, 10))
-continents   <-  unique(data_1982$continent)
-
-pdf('./figure1.pdf', width = 7, height = 7)
-plot(lifeExp ~ gdpPercap, data_1982, log = 'x', cex = cex, col = col, pch = 21, las = 1, xlab = 'GDP per capita', ylab = 'Life expectancy (years)', main = 1982)
-for (i in seq_along(continents)) {
-    f(data = data_1982, continent = continents[i], color_table = color_table)
-}
-dev.off()
+makeFigure1(data_1982, output_filename = 'output/figure1.pdf', main = '1982')
+makeFigure1(data, output_filename = 'output/figure2.pdf', main = 'whole time series')
